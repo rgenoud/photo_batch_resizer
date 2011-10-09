325af790a1fe3bb135d2d242b0bd5cb4127982c0
Richard Genoud <richard.genoud@gmail.com>
2011-10-09 14:46:40 +0200

    indent

--- image.py
+++ image.py
@@ -22,38 +22,38 @@ console.setFormatter(formatter)
 logger.addHandler(console)
 
 def do_scale(value) :
-	global scale_factor
-	return ((value * scale_factor) / 100)
+    global scale_factor
+    return ((value * scale_factor) / 100)
 
 
 def resize(in_file, out_file) :
-	try :
-		image = Image.open(in_file, mode = 'r')
-		if (image and image.format) :
-			logger.info("treat file : " + in_file + " format=" + image.format)
-			new_size = map(do_scale, image.size)
-			image.thumbnail(new_size)
-			image.save(out_file, image.format)
-			return
-	except :
-		logger.error("unable to open file " + in_file)
+    try :
+        image = Image.open(in_file, mode = 'r')
+        if (image and image.format) :
+            logger.info("treat file : " + in_file + " format=" + image.format)
+            new_size = map(do_scale, image.size)
+            image.thumbnail(new_size)
+            image.save(out_file, image.format)
+            return
+    except :
+        logger.error("unable to open file " + in_file)
 
 class Thread_resize(threading.Thread) :
-	def __init__(self, to_do) :
-		threading.Thread.__init__(self)
-		self.__to_do = to_do
+    def __init__(self, to_do) :
+        threading.Thread.__init__(self)
+        self.__to_do = to_do
 
-	def run(self) :
-		logger.debug("starting thread " + self.getName())
-		try :
-			while (True) :
-				value = self.__to_do.pop()
-				resize(value[0], value[1])
-		except :
-			# end of list
-			pass
-		finally :
-			logger.debug("stopping thread " + self.getName())
+    def run(self) :
+        logger.debug("starting thread " + self.getName())
+        try :
+            while (True) :
+                value = self.__to_do.pop()
+                resize(value[0], value[1])
+        except :
+            # end of list
+            pass
+        finally :
+            logger.debug("stopping thread " + self.getName())
 
 
 to_do = []
@@ -64,24 +64,24 @@ dirList = os.listdir(in_dir)
 
 # list of files to process
 for i in dirList :
-	logger.debug("found file " + i)
-	in_file = os.path.join(in_dir, i)
-	if (os.path.isfile(in_file)) : 
-		out_filename = os.path.splitext(i)[0] + '_small' + os.path.splitext(i)[1]
-		out_file = os.path.join(out_dir, out_filename)
-		to_do.append((in_file, out_file))
-		logger.debug("in_file=" + in_file + " out_file=" + out_file)
+    logger.debug("found file " + i)
+    in_file = os.path.join(in_dir, i)
+    if (os.path.isfile(in_file)) : 
+        out_filename = os.path.splitext(i)[0] + '_small' + os.path.splitext(i)[1]
+        out_file = os.path.join(out_dir, out_filename)
+        to_do.append((in_file, out_file))
+        logger.debug("in_file=" + in_file + " out_file=" + out_file)
 
 # create the threadpool
 for i in range(cpu_count) :
-	jobs.append(Thread_resize(to_do))
+    jobs.append(Thread_resize(to_do))
 
 # start the threads
 for i in range(len(jobs)) :
-	jobs[i].start()
+    jobs[i].start()
 
 # wait for all thread to finish
 for i in range(len(jobs)) :
-	if (jobs[i].is_alive()) :
-		jobs[i].join()
+    if (jobs[i].is_alive()) :
+        jobs[i].join()
 
